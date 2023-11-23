ddf106ca49fa488c83e368876986c96b6e72877a
DaniParkHoJin <parkhojin@naver.com>
2023-11-23 13:22:40 +0900

    관리자 구성

--- src/main/java/org/parkhojin/commons/constans/MemberType.java
+++
@@ -1,6 +0,0 @@
-package org.parkhojin.commons.constans;
-
-public enum MemberType {
-    USER, // 일반 회원
-    ADMIN // 관리자
-}
--- src/main/java/org/parkhojin/commons/exceptions/AlertBackException.java
+++
@@ -1,9 +0,0 @@
-package org.parkhojin.commons.exceptions;
-
-
-public class AlertBackException extends AlertException {
-
-    public AlertBackException(String message) {
-        super(message);
-    }
-}
