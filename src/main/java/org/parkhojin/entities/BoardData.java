06c445ba24ae576380869b9a2f5d928792c16871
DaniParkHoJin <parkhojin@naver.com>
2023-12-02 15:20:04 +0900

    권한 체크 끝

--- src/main/java/org/parkhojin/entities/BoardData.java
+++ src/main/java/org/parkhojin/entities/BoardData.java
@@ -12,24 +12,34 @@ import java.util.UUID;
 @Entity
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
-public class BoardData extends Base{
+@Table(indexes = { @Index(name = "idx_bd_data_list", columnList = "notice DESC, createdAt DESC"),
+@Index(name = "idx_bd_category", columnList = "category")
+})
+public class BoardData extends Base {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long seq;
 
     @Column(length = 50, nullable = false)
     private String gid = UUID.randomUUID().toString();
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="bId")
+    @JoinColumn(name = "bId")
     private Board board;
 
-    @ManyToOne(fetch= FetchType.LAZY)
-    @JoinColumn(name="userNo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userNo")
     private Member member;
+
+    @Column(length = 50)
+    private String category;
 
     @Column(length = 30, nullable = false)
     private String poster;
+
+    @Column(length = 45)
+    private String guestPw; // 비회원 비밀번호
 
     @Column(nullable = false)
     private String subject;
